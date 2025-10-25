420f08ffc4987184f4ea342150d4df1240bc93e0
jorgetajin5 <jorgetajin5@gmail.com>
2025-10-25 11:19:06 -0600

    Arreglos en la estructura

--- src/config/languages/python.ts
+++ src/config/languages/python.ts
@@ -14,7 +14,6 @@ export const pythonLanguage = {
 
   grouping: ["\\(", "\\)", "\\[", "\\]", "\\{", "\\}", ",", ":", ";", "\\."],
 
-
   comment: {
     single: "#.*",
     multi: '""".*?"""|\'\'\'.*?\'\'\''
